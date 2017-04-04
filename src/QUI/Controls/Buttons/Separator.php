d35c21b5872e5843cb4fe03393203b3f5218a616
Henning Leutz <leutz@pcsg.de>
2017-04-04 15:26:44 +0200

    fix: Separator Classes

---
+++ src/QUI/Controls/Buttons/Separator.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * This file contains the \QUI\Controls\Buttons
+ */
+
+namespace QUI\Controls\Buttons;
+
+use QUI;
+
+/**
+ * Button Separator
+ *
+ * @author  www.pcsg.de (Henning Leutz)
+ * @package com.pcsg.qui.controls.buttons
+ */
+class Separator extends QUI\QDOM
+{
+    /**
+     * the Parent Object
+     *
+     * @var \QUI\Controls\Toolbar\Bar
+     */
+    private $Parent;
+
+    /**
+     * Constructor
+     *
+     * @param array $settings
+     */
+    public function __construct(array $settings)
+    {
+        $this->setAttribute('type', 'QUI\\Controls\\Buttons\\Separator');
+        $this->setAttributes($settings);
+    }
+
+    /**
+     * Set the Parent
+     *
+     * @param \QUI\Controls\Toolbar\Bar $Parent
+     */
+    public function addParent($Parent)
+    {
+        $this->Parent = $Parent;
+    }
+
+    /**
+     * get the name attribute
+     *
+     * @return String
+     */
+    public function getName()
+    {
+        return $this->getAttribute('name');
+    }
+
+    /**
+     * Ertstellt den JavaScript Code und ruft die create Methode auf
+     *
+     * @return String
+     */
+    public function create()
+    {
+        return 'var ' . $this->getAttribute('name') . ' = '
+               . $this->jsObject() . ';'
+               . $this->Parent->getName() . '.appendChild( '
+               . $this->getAttribute('name') . ' );';
+    }
+
+    /**
+     * Ertstellt den JavaScript
+     *
+     * @return String
+     */
+    public function jsObject()
+    {
+        $jsString = 'new QUI.controls.buttons.Separator({';
+
+        if ($this->getAttribute('height')) {
+            $jsString .= 'height: "' . $this->getAttribute('height') . '",';
+        }
+
+        $jsString .= 'name: "' . $this->getAttribute('name') . '"';
+        $jsString .= '})';
+
+        return $jsString;
+    }
+}
